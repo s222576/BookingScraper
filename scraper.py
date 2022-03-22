64681d4516c23e504fd72e6ccd2849413a06cdfc
s-013-13 <s-013-13@gumiforest.ae.krakow.pl>
2022-03-22 12:59:42 +0100

    2022-03-22 KW

--- scraper.py
+++ scraper.py
@@ -1,7 +1,10 @@
-from urllib import response
 import requests
+from bs4 import BeautifulSoup
 
-url = "https://www.booking.com/hotel/pl/happy-tower.html"
+url = "https://www.booking.com/hotel/pl/happy-tower.uk.html"
 response = requests.get(url)
-print(response.status_code)
-print(response.text)
+
+page_dom = BeautifulSoup(response.text, 'html.parser')
+
+reviews = page_dom.select('li.review_list_new_item_block')
+print(len(reviews))
